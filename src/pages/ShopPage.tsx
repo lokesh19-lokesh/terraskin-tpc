d1ad0f86af8dd0fe7ae0953b950d285db170d859
lokesh19-lokesh <lokeshmaddibiona@gmail.com>
2026-04-11 16:49:08 +0530

    j

--- src/pages/ShopPage.tsx
+++ src/pages/ShopPage.tsx
@@ -215,7 +215,7 @@ const ShopPage: React.FC = () => {
                 </p>
               </div>
 
-              <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-6">
+              <div className="grid grid-cols-2 xl:grid-cols-3 gap-4 md:gap-6">
                 {filteredProducts.map((product, index) => (
                   <AnimatedSection key={product.id} delay={index * 100} animation="slide-up">
                     <ProductCard product={product} />
